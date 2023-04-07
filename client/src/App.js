b8389ecdc42ffc4e94dd6ffcdede296641ab022a
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-04-07 14:06:49 -0300

    created an Apollo Provider to make every request work with the Apollo server.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/clent/link/context";
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
+const authLink = setContext ((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+   const token = localStorage.getItem("id_token");
+   // return the headers to the context so httpLink can read them
+   return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+   };
+});
+
+const httpLink = createHttpLink({
+  uri:"/graphql",
+});
+// setting client to execute the `authLink` middleware prior to making the request to our GraphQL API
+const client = new ApolloClient({
+   link: authLink.concat (httpLink),
+   cache: new InMemoryCache(),
+});
 
 function App() {
   return (
