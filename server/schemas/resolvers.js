49838741fc7a688a068b34e2914bfffc568c101d
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-04-07 00:19:25 -0300

    added resolvers for mutation to add and remove books

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -32,7 +32,34 @@ const resolvers = {
               if (!correctPW) {
                 throw new AuthenticationError("Wrong credentials!");
               };
-              
-            }
+              const token = signToken(user);
+              return { token, user };
+            },
+        saveBook: async (parent, { bookData }, context) => {
+            if(context.user) {
+                const updateUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: bookData } },
+                    { new: true },
+                )
+                .populate("books");
+            return updateUser;
+            };
+            throw new AuthenticationError("User must be logged in to save books");
+        },
+
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user,_id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true },
+            );
+             return updatedUser;
+        };
+        throw new AuthenticationError("User must be logged in to delete books");
         }
-    }
+    },
+};
+
+module.exports = resolvers;
